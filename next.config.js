314dabfca032b0779e490d2a2dd678b5a5a07683
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-07-05 19:31:41 -0300

    feat: add trip item component

--- next.config.js
+++ next.config.js
@@ -9,7 +9,13 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
                 port: '',
-            }
+            },
+
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                port: "",
+              },
         ]
     }
 }
