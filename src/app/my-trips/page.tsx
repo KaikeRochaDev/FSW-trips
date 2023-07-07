9aa17aa8cbc1075454b767318673ed371dc4e5d0
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-07-07 18:38:45 -0300

    feat: add page reload on trip cancelation

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -10,7 +10,7 @@ import UserReservationItem from "./components/UserReservationItem";
 import Button from "@/components/Button";
 
 const MyTrips = () => {
-    const [reservations, setReservations] = useState<
+  const [reservations, setReservations] = useState<
     Prisma.TripReservationGetPayload<{
       include: { trip: true };
     }>[]
@@ -20,30 +20,41 @@ const MyTrips = () => {
 
   const router = useRouter();
 
+  const fetchReservations = async () => {
+    const response = await fetch(
+      `http://localhost:3000/api/user/${(data?.user as any)?.id}/reservations`
+    );
+
+    const json = await response.json();
+
+    setReservations(json);
+  };
   useEffect(() => {
-    if (status === "unauthenticated" || !data?.user) {
+    if (status === "unauthenticated") {
       return router.push("/");
     }
-
-    const fetchReservations = async () => {
-      const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`);
-
-      const json = await response.json();
-
-      setReservations(json);
-    };
 
     fetchReservations();
   }, [status]);
 
   return (
     <div className="container mx-auto p-5">
-      <h1 className="font-semibold text-primaryDarker text-xl">Minhas Viagens</h1>
+      <h1 className="font-semibold text-primaryDarker text-xl">
+        Minhas Viagens
+      </h1>
       {reservations.length > 0 ? (
-        reservations?.map((reservation) => <UserReservationItem key={reservation.id} reservation={reservation} />)
+        reservations?.map((reservation) => (
+          <UserReservationItem
+            fetchReservations={fetchReservations}
+            key={reservation.id}
+            reservation={reservation}
+          />
+        ))
       ) : (
         <div className="flex flex-col">
-          <p className="mt-2 font-medium text-primaryDarker">Você ainda não tem nenhuma reserva! =(</p>
+          <p className="mt-2 font-medium text-primaryDarker">
+            Você ainda não tem nenhuma reserva! =(
+          </p>
 
           <Link href="/">
             <Button className="w-full mt-2">Fazer reserva</Button>
