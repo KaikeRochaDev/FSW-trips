f0f7f636cd4963ab99895cfb07cb7de98553afd6
KaikeRochaDev <kaikerochadev@outlook.com>
2023-09-23 11:14:07 -0300

    Returning with the primary code [reservatioId]

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -1,26 +1,21 @@
 import { prisma } from "@/lib/prisma";
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextResponse } from "next/server";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'DELETE') {
-    const reservationId = req.query.reservationId as string;
-
-    if (!reservationId) {
-      return res.status(400).json({ message: "Missing reservationId" });
-    }
-
-    try {
-      const reservation = await prisma.tripReservation.delete({
-        where: {
-          id: reservationId,
-        },
-      });
-
-      return res.status(200).json(reservation);
-    } catch (error) {
-      return res.status(500).json({ message: "Error deleting reservation" });
-    }
-  } else {
-    return res.status(405).end();
+export async function DELETE(_request: Request, { params: { reservationId } }: { params: { reservationId: string } }) {
+  if (!reservationId) {
+    return {
+      status: 400,
+      body: {
+        message: "Missing reservationId",
+      },
+    };
   }
+
+  const reservation = await prisma.tripReservation.delete({
+    where: {
+      id: reservationId,
+    },
+  });
+
+  return new NextResponse(JSON.stringify(reservation), { status: 200 });
 }
